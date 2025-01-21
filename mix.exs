22d986917e83db802cda82d4a2540768d16102c9
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2025-01-21 12:32:02 +0100

    Library elixir version set to 1.14

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule GenRMQ.Mixfile do
     [
       app: :gen_rmq,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
